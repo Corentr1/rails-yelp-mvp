e4e978f785c9381e3658e3f4acbf20d61b4e8f74
Corentin Peyresblanques <corentin.peyresblanques@gmail.com>
2022-02-16 15:15:53 +0100

    final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+restaurant = Restaurant.new(name:'La Fourchette', address:'Brussels', phone_number:'0412156482', category:'chinese')
+restaurant_one = Restaurant.new(name:'Chtuc', address:'Paris', phone_number:'0718549760', category:'belgian')
+restaurant_two = Restaurant.new(name:'Bourg-Palette', address:'Kanto', phone_number:'0864156482', category:'italian')
+restaurant_three = Restaurant.new(name:'Oblivion', address:'Cyrodil', phone_number:'0546982154', category:'french')
+restaurant_four = Restaurant.new(name:'Le Couteau', address:'Brussels', phone_number:'0852156482', category:'chinese')
+
+restaurant.save
+restaurant_one.save
+restaurant_two.save
+restaurant_three.save
+restaurant_four.save
